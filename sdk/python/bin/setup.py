19abfe7a43c19f1d69a198e105a3ead07aff6b6b
Tim <6519758+bradleybluebean@users.noreply.github.com>
2023-08-28 17:18:27 -0700

    feat: release v0.0.2

--- sdk/python/bin/setup.py
+++ sdk/python/bin/setup.py
@@ -8,8 +8,8 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "1.0.0a21+dirty"
-PLUGIN_VERSION = "1.0.0-alpha.21+dc3a712b.dirty"
+VERSION = "0.0.2"
+PLUGIN_VERSION = "0.0.2"
 
 class InstallPluginCommand(install):
     def run(self):
